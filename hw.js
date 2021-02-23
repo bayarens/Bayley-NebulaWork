9e4b54a07818dcfaa98d07d1f09fef8208097083
bayarens <bay.arens@gmail.com>
2021-02-23 17:55:38 -0500

    hw.js

---
+++ hw.js
@@ -0,0 +1,33 @@
+let x = "bayley";
+
+console.log(x);
+
+let today = "tuesday";
+
+console.log(today);
+
+let carName = "Volvo";
+
+console.log(carName);
+
+let num = "50"; 
+
+console.log(num);
+
+let num1 = 5; 
+let num2 = 10; 
+
+let num3 = num1+num2;  
+
+console.log(num3);
+
+const myDog = {
+    name: 'appa',
+    color: 'brown',
+    age: '3',
+    weight: '70lbs',
+    height: '3ft',
+    favoriteFood: 'burger'
+}
+
+console.log(myDog)
---
+++ script.js
@@ -0,0 +1,3 @@
+let variable = "good work today guys";
+
+console.log(variable); 
