32c0137a46be1c52e83623047ee3bf259d81b4d1
bayarens <bay.arens@gmail.com>
2021-03-03 10:00:16 -0500

    Bayley-NebulaWork

---
+++ ClassWorkFolder/02-24.js
@@ -0,0 +1,19 @@
+let x = "hello" 
+let y = "goodbye" 
+
+console.log(x+y)
+
+let string = "Hello friend";
+console.log(string.length) 
+
+console.log(string.slice(6,12)) 
+
+const hello = "Hello"
+const world = "World"
+const my = "My"
+const name = "name";
+const is = "is"; 
+let myName = "Bayley" 
+let template = `${hello} ${world} ${my} ${name} ${is}`
+
+console.log(template, myName)
---
+++ ClassWorkFolder/02-25.js
@@ -0,0 +1,3 @@
+console.log("my favorite icecream is" + " " + "mango")
+// or ^ this one is the more efficent way
+console.log("my favorite icecream is ".concat("mango"))  
