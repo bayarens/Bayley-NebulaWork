6a8f8015759f36648f2a75898473a1ae360cdd02
bayarens <bay.arens@gmail.com>
2021-03-04 15:45:45 -0500

    FizzBuzzAsignment.js

--- ClassWorkFolder/FizzBuzzAsignment.js
+++ ClassWorkFolder/FizzBuzzAsignment.js
@@ -1,9 +1,9 @@
 // Function that will print 1-100
 //          for(let i = 1; i<101; i++)
 
-for(let i = 1; i<101; i++){
-    if(i%3 == 0 && i%5 ==0){
-        console.log(" fizz buzz")
+for(let i = 1; i<=100; i++){
+    if(i%3 === 0 && i%5 === 0){
+        console.log("fizz buzz")
     } else if (i%3 ===0) {
         console.log("fizz")
     } else if (i%5 ===0) {
