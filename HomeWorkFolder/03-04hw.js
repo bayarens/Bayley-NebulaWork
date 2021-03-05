f023eda493ef487585afd40fa748ecf017772446
bayarens <bay.arens@gmail.com>
2021-03-05 11:48:35 -0500

    03-04hw.js

--- HomeWorkFolder/03-04hw.js
+++ HomeWorkFolder/03-04hw.js
@@ -25,35 +25,36 @@ arrSum(numArray)
 3. Create a function that accepts an array and a search value. This function will return true if the search value is an element in the array. Otherwise, it will return false. Don't use any built in array methods to do this. Ie: 
 doesItInclude([23, 24, "kale"], "kale")  -> should return true
 */
-console.log(searchFunction([23, 24, "kale"], "kale"))
+
 function searchFunction(array, searchValue) {
     for (let i = 0; i < array.length; i++) {
         if (searchValue == array[i]) {
             return true
         }
     }
+    return false
 }
-
+console.log(searchFunction([23, 24, "kale"], "kale"))
 
 // 4. Create a function that accepts an array. This function will return a new array with the elements in reverse order.
 function reverseArr(array) {
     let flipper = []
-    let jlipper = 0
-    for (let i = array.length; i > 0; i--) {
-        flipper[i] = array[jlipper++]
+
+    for (let i = array.length - 1; i >= 0; i--) {
+        flipper.push(array[i])
     }
-    flipper.shift()
     console.log(flipper)
     return flipper
-}
+} 
 reverseArr(numArray)
 // 5. Create another function that adds the numbers in the array that are NOT divisible by 3 and return the total.
 
 function sumNotDiv(array) {
     let sum = 0
     for (let i = 0; i < array.length; i++) {
-        if (array[i] % 3 == 0){
-        sum = sum + array[i]
+        if (array[i] % 3 !== 0) {
+            sum = sum + array[i]
+            //or += array[i] 
         }
     }
     return sum
