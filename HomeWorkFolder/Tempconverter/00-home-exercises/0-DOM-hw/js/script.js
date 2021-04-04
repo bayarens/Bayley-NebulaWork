68122b70892f8599303ad97c496ad5420908121f
bayarens <bay.arens@gmail.com>
2021-04-04 17:03:21 -0400

    temp converter

---
+++ HomeWorkFolder/Tempconverter/00-home-exercises/0-DOM-hw/js/script.js
@@ -0,0 +1,77 @@
+// Save a reference to temperature input element
+const enterTemp = document.querySelector('#enter-temp > input'); 
+
+// Save a reference to Fahrenheit output
+const fahOutput = document.querySelector('#fahrenheit-output');
+
+// Save a reference to Celsius output
+const celOutput = document.querySelector('#celsius-output'); 
+
+// Save a reference to the Kelvin output
+const kelOutput = document.querySelector('#kelvin-output'); 
+
+// Save a reference to all radio buttons - read up on querySelectorAll
+// Be sure to try and use the right css selector - use W3 schools CSS selector reference
+const allRadioButtons = document.querySelectorAll('input[type = radio]');
+
+// Save a reference to submit button
+const submitBtn = document.querySelector('button');
+
+const userOutput = {
+    celsius: null,
+    fahrenheit: null,
+    kelvin: null
+}
+
+// ALREADY DONE
+// Fahrenheit to Celsius formula: c = (f - 32) / 1.8
+// Fahrenheit to Kelvin formula: k = (f + 459.67) / 1.8
+const calculateForCelsius = (c) => {
+    userOutput.celsius = c;
+    userOutput.fahrenheit = c * 1.8 + 32;
+    userOutput.kelvin =  c + 273.15;
+}
+
+// ALREADY DONE
+// Kelvin to Fahrenheit formula: f = k * 1.8 - 459.67
+// Kelvin to Celsius formula: c = k - 273.15
+const calculateForKelvin = (k) => {
+    userOutput.celsius = k - 273.15;
+    userOutput.fahrenheit = k * 1.8 - 459.67;
+    userOutput.kelvin = k;
+}
+
+// ALREADY DONE
+// Celsius to Fahrenheit formula: f = c * 1.8 + 32
+// Celsius to Kelvin formula: k = c + 273.15
+const calculateForFahrenheit = (f) => {
+    userOutput.celsius = (f - 32) / 1.8;
+    userOutput.fahrenheit = f;
+    userOutput.kelvin = (f + 459.67) / 1.8;
+}
+
+const userUnitChosen = () => {
+    for(let i = 0; i < allRadioButtons.length; i++){
+        if(allRadioButtons[i].checked) return allRadioButtons[i].value;
+    }
+}
+
+const setOutput = () => {
+    // Here we want to use our userOutput object to populate our conversions in the DOM
+    fahOutput.innerText = userOutput.fahrenheit
+    celOutput.innerText = userOutput.celsius
+    kelOutput.innerText = userOutput.kelvin
+}
+
+const populateUserOutput = (input) => {
+    console.log(input)
+    const unitChosen = userUnitChosen();
+    if(unitChosen === "Fahrenheit") calculateForFahrenheit(input);
+    if(unitChosen === "Celsius") calculateForCelsius(input);
+    if(unitChosen === "Kelvin") calculateForKelvin(input);
+    setOutput();
+}
+
+// We need to add an event listener to our submit button that waits for the user to click on it.
+submitBtn.addEventListener("click", () => populateUserOutput(enterTemp.value))
+// After the click, we want to populate the user output section of our page.
