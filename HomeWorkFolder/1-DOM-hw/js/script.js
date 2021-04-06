a865bbea205d50bbaa00368e45389429f3f82977
bayarens <bay.arens@gmail.com>
2021-04-06 11:51:49 -0400

    corrected button file, command correct not pointing to the right tag tho

--- HomeWorkFolder/1-DOM-hw/js/script.js
+++ HomeWorkFolder/1-DOM-hw/js/script.js
@@ -6,42 +6,19 @@ const yellowBtn = document.querySelector('#yellowButton');
 // Extra credit: make the border bigger on the selected circle
 // Extra credit: add a reset button - turns background white
 
-function redScheme () {
+redBtn.addEventListener('click', function redScheme () {
     document.querySelector("body").className = "red-scheme";
-    redBtn.addEventListener('click', () => {
-        const curColour = document.body.style.backgroundColor;
-        if(curColour !== 'red'){
-            document.body.style.backgroundColor = "red";
-        }
-    })
-};
+    }) 
 
-function skyBlueScheme(){
+ blueBtn.addEventListener('click', function skyBlueScheme () {
     document.querySelector("body").className = "skyblue-scheme";
-    blueBtn.addEventListener('click', () => {
-        const curColour = document.body.style.backgroundColor;
-        if(curColour !== 'skyblue'){
-            document.body.style.backgroundColor = "skyBlue";
-        }
-    })
-};
+    })  
 
-function yellowScheme(){
+yellowBtn.addEventListener('click', function yellowScheme () {
     document.querySelector("body").className = "yellow-scheme";
-    yellowBtn.addEventListener('click', () => {
-        const curColour = document.body.style.backgroundColor;
-        if(curColour !== 'yellow'){
-            document.body.style.backgroundColor = "yellow";
-        }
     })
-};
 
-function tealScheme(){
+
+tealBtn.addEventListener('click', function tealScheme(){
     document.querySelector("body").className = "teal-scheme";
-    tealBtn.addEventListener('click', () => {
-        const curColour = document.body.style.backgroundColor;
-        if(curColour !== 'teal'){
-            document.body.style.backgroundColor = "teal";
-        }
     })
-};
