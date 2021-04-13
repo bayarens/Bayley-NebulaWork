f460206d48b47a29b7c5a8bb0c99c7ce43f1fe1a
bayarens <bay.arens@gmail.com>
2021-04-13 10:27:47 -0400

    reorganized and gave display boxes overflow

--- Calculator-project/script.js
+++ Calculator-project/script.js
@@ -7,29 +7,24 @@ const hideButton = document.querySelector("#hide-button")
 
 const calculator = document.querySelector("#calculator-frame")
 
-// this is the event listener that that will change the hide button to show when it is pressed 
-hideButton.addEventListener("click", onToggleHideClick)
+//references for my buttons 
+const numKeys = document.querySelectorAll("button.num")
 
-function onToggleHideClick(e){
-    calculator.classList.toggle('hide');
-    e.target.innerText = calculator.classList.contains('hide') ? 'Show' : 'Hide'
-}
-// these are all my operations so when I hit a operation it will take will do the math on the 
-//numbers in the display box and display box holder by running them through the calculate function
-let operator = ""
+const operKeys = document.querySelectorAll("button.opp")
 
-const operators = {
-    "+": () => +displayBoxHolder.value + +displayBox.value,
-    "-": () => displayBoxHolder.value - displayBox.value,
-    "*": () => displayBoxHolder.value * displayBox.value,
-    "/": () => displayBoxHolder.value / displayBox.value,
-    "^": () => displayBoxHolder.value ** displayBox.value,
-}
-//this is the function that will take take the buttons ive pressed (if they are numbers) and populate the display box
+const clearKey = document.querySelector("#clear")
+
+const equalKey = document.querySelector("#equal")
+
+//populating display box
+numKeys.forEach(numKey => numKey.addEventListener("click", () => populateDisplayBox(numKey)));
+
+//this is the function that will take take the buttons I've pressed (if they are numbers) and populate the display box
 const populateDisplayBox = (button) => {
     displayBox.value += button.innerText
     clearKey.innerText = "C"
 }
+
 // this is the function that will take what was in the display box and put it into the display box below to hold the value after an operation is entered
 const populateDisplayBoxHolder = (button) => {
     displayBoxHolder.value = displayBox.value
@@ -43,8 +38,24 @@ const populateDisplayBoxHolder = (button) => {
 }
 
 let activeButton 
-//this is the function that actually does the calculation(line 51 is makes the display box value be the singular "operation" of the "operators" and running 
-//that through the calculate function as a paramater)
+
+// these are all my operations so when I hit a operation it will take will do the math on the 
+//numbers in the display box and display box holder by running them through the calculate function
+let operator = ""
+
+const operators = {
+    "+": () => +displayBoxHolder.value + +displayBox.value,
+    "-": () => displayBoxHolder.value - displayBox.value,
+    "*": () => displayBoxHolder.value * displayBox.value,
+    "/": () => displayBoxHolder.value / displayBox.value,
+    "^": () => displayBoxHolder.value ** displayBox.value,
+}
+
+// want to write code that says, after I hit an opperator key, whatever was in the display box should be pushed to the display box holder and then 
+console.log(operKeys)
+operKeys.forEach(operKey => operKey.addEventListener("click", () => populateDisplayBoxHolder(operKey)));
+
+//this is the function that actually does the calculation
 const calculate = () => {
     if(!operator){
         return
@@ -55,18 +66,8 @@ const calculate = () => {
     activeButton = null
     operator = null
 }
-
-//references for my buttons 
-const numKeys = document.querySelectorAll("button.num")
-
-const operKeys = document.querySelectorAll("button.opp")
-
-const clearKey = document.querySelector("#clear")
-
-const equalKey = document.querySelector("#equal")
-
-//populating display box
-numKeys.forEach(numKey => numKey.addEventListener("click", () => populateDisplayBox(numKey)));
+//added event listener to clear key and called the calculate function
+equalKey.addEventListener("click", calculate)
 
 //the clear key function, also changed it from A/C to C
 function clear() {
@@ -76,7 +77,7 @@ function clear() {
         displayBoxHolder.value = ""
     }
     clearKey.innerText = "A/C"
-    if(activeButton){
+    if (activeButton){
         activeButton.classList.remove("active")
     }
     activeButton = null
@@ -85,15 +86,15 @@ function clear() {
 
 clearKey.addEventListener("click", clear)
 
-// want to write code that says, after I hit an opperator key, whatever was in the display box should be pushed to the display box holder and then 
-console.log(operKeys)
-operKeys.forEach(operKey => operKey.addEventListener("click", () => populateDisplayBoxHolder(operKey)));
+// this is the event listener that that will change the hide button to show when it is pressed 
+hideButton.addEventListener("click", onToggleHideClick)
+
+function onToggleHideClick(e){
+    calculator.classList.toggle('hide');
+    e.target.innerText = calculator.classList.contains('hide') ? 'Show' : 'Hide'
+}
 
 
-// want to write code that will take what I have in the display box and do whatever opperation was chose to the display box holder with the content in display box being a paramater
 
-//equalKey.addEventListener("click", () => displayBox  operate  displayBoxHolder => populate display box
-
-equalKey.addEventListener("click", () => calculate())
 
 
